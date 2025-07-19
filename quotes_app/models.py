8e4fb5c4346426e68764ef4b500cd232b24937e6
andreylapko1 <andreycgo765@gmail.com>
2025-07-19 15:38:15 +0300

    add top 10 view

--- quotes_app/models.py
+++ quotes_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,16 +6,39 @@ class Quote(models.Model):
     quote_text = models.CharField(verbose_name='Quote Text', null=False, blank=False, max_length=500)
     quote_author = models.CharField(verbose_name='Quote Author',null=True, blank=True ,max_length=200)
     category = models.CharField(verbose_name='Quote Category',null=True, blank=True ,max_length=200)
+    likes = models.IntegerField(verbose_name='Quote Likes', null=False, blank=False, default=0)
+    dislikes = models.IntegerField(verbose_name='Quote Dislikes', null=False, blank=False, default=0)
+    views = models.IntegerField(verbose_name='Quote Views', null=False, blank=False, default=0)
+
+    @property
+    def weight(self):
+        BASE_WEIGHT = 5
+        return max(1, BASE_WEIGHT + (self.likes - self.dislikes))
 
 
     class Meta:
+        ordering = ['-likes']
         verbose_name = 'Quote'
         verbose_name_plural = 'Quotes'
-        ordering = ['quote_author']
 
 
     def __str__(self):
         return f'{self.quote_text} - {self.quote_author}'
 
+
+
+class UserQuoteFeedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE, verbose_name='Quote')
+    type = models.CharField(verbose_name='Feedback type', choices=[('like', 'Like'), ('dislike', 'Dislike')],)
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Timestamp')
+
+    class Meta:
+        unique_together = ('user', 'quote')
+        verbose_name = 'Feedback'
+        verbose_name_plural = 'Feedbacks'
+
+    def __str__(self):
+        return f'{self.user.username} - {self.quote}'
 
 # Create your models here.
--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,31 +1,52 @@
-from django.shortcuts import render
+import random
+
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-import requests
-from .models import Quote
+from .models import Quote, UserQuoteFeedback
 from quotes_app.utils.get_quote import get_quote
 
 
 class BaseView(View):
     def get(self, request):
-        attempts = 10
-        for _ in range(attempts):
-            context = get_quote()
-            if not context:
-                return render(request, 'base.html', context={'quote_text':'Error. The service is temporarily unavailable'})
-            text = context.get('quote_text')
-            author = context.get('quote_author')
-            if not text:
-                return render(request, 'base.html', context={'quote_text':'Error. The quote text is empty'})
+        quotes = Quote.objects.all()
+        if get_quote:
+            weighted_quotes = []
+            for quote in quotes:
+                weight = quote.weight
+                weighted_quotes.extend( [quote] * weight)
+            random_quote = random.choice(weighted_quotes)
+            random_quote.views += 1
+            random_quote.save()
+            context = {
+                'quote': random_quote
+            }
+            return render(request, 'quotes_app/base.html', context=context)
+        return render(request, 'quotes_app/base.html', context={'quote_text': 'Error'})
 
-            if not Quote.objects.filter(quote_text=text).exists():
-                if Quote.objects.filter(quote_author=author).count() <= 3:
-                    quote = Quote(**context)
-                    quote.save()
-                    return render(request, 'quotes_app/base.html', context=context)
-        return render(request, 'quotes_app/base.html', context={'quote_text':'Error. Not found'}, status=404)
+    def post(self, request, quote_id, action):
+        quote = get_object_or_404(Quote, pk=quote_id)
+        if action == 'like':
+            quote.likes += 1
+        elif action == 'dislike':
+            quote.dislikes += 1
+        elif action == 'unlike':
+            if quote.likes > 0:
+                quote.likes -= 1
+        elif action == 'undislike':
+            if quote.dislikes > 0:
+                quote.dislikes -= 1
+        else:
+            return JsonResponse({'error': 'Invalid action'})
+        quote.save()
+        return JsonResponse({'status': 'success', 'likes': quote.likes, 'dislikes': quote.dislikes})
 
 
 
+class PopularQuotesView(View):
+    def get(self, request):
+        quotes = Quote.objects.all()[:10]
+        return render(request, 'quotes_app/popular_quotes.html', context={'quotes': quotes})
 
 
 
